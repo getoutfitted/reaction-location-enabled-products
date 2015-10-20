7b90c03d402bf5a6d640fd78f334669afbccfe57
Spencer Norman <spencern@gmail.com>
2015-10-20 13:17:50 -0600

    Add event helper to handle settings buttons.

--- client/templates/dashboard.js
+++ client/templates/dashboard.js
@@ -14,6 +14,22 @@ Template.locationEnabledProducts.helpers({
   }
 });
 
+Template.locationEnabledProducts.events({
+  'click .pkg-settings, click [data-event-action=showPackageSettings]': function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    const pkgData = {
+      label: 'Location Enabled Products Settings',
+      route: 'dashboard/location-enabled-products',
+      provides: 'settings',
+      container: 'dashboard',
+      template: 'locationEnabledProductsSettings'
+    };
+    // Show the advanced settings view using this package registry entry
+    ReactionCore.showActionView(pkgData);
+  }
+});
+
 AutoForm.hooks({
   'location-enabled-products-update-form': {
     onSuccess: function () {
